4138b6890ad167f5c9cb508e86d6e77922cb85b7
Hannes Nickisch <hannes.nickisch@philips.com>
2022-12-07 19:03:49 +0100

    Version running with NaN replaced by mean.

--- estimador/estimador_IC50_TAS_120.m
+++ estimador/estimador_IC50_TAS_120.m
@@ -1,50 +1,56 @@
 % run startup.m prior
 % activity estimator for TAS-120 against erbB-2
 clear all, close all
-clc
+pkg load statistics
+rand('seed',0)
 
 %% build the regressors
 
-load Xt.txt        %Xtraining --> ligands properties: QED, CX Acidic pKa,	CX Basic pKa,	HBA,	HBD,	PSA
-load yt.txt        %Ytraining --> IC50
-load Xtest.txt     %TAS-120's properties
+load Xt.txt        % Xtraining --> ligands properties: QED, CX Acidic pKa, CX Basic pKa,	HBA,	HBD,	PSA
+load yt.txt        % Ytraining --> IC50
+load Xtest.txt     % TAS-120's properties
 
-%retardo = 1; %delay
+% replace NaN values by mean
+xm = nanmean(Xt);
+for ii=1:6
+  Xt(isnan(Xt(:,ii)),ii) = xm(ii);
+  Xtest(isnan(Xtest(:,ii)),ii) = xm(ii);
+end
 
-%xd = delay(xent,retardo);           %if desired
+% yt = log(yt); % work in the log domain
+yt = yt.^(1/7); % work in the power domain
+
+% subsample training set
+ii = randperm(size(Xt,1));
+n = 1000;
+Xt = Xt(ii(1:n),:);
+yt = yt(ii(1:n));
+
+%retardo = 1; % delay
+
+%xd = delay(xent,retardo);           % if desired
 %xd2 = delay(xd,retardo);
 
-startup      %call the GPML toolbox
-meanfunc = {@meanZero}; %this function has not hyp.mean
-%feval(covfunc{:}) % how many hyperparametrs has meanfunc, covfunc or likfunc
-SE = {@covSEard};
-L = rand(6,1); sf = 2; hypSE = log([L;sf]);
-LIN = {'covLINard'};
-L = rand(6,1); hypLIN = (L);
-CONS = {@covConst};
-sf = 2;  hypCONS = log(sf); %hypCONS = 0;
-NO = {'covNoise'};
-sn = .2;  hypNO = log(sn); %hypNO = 0;
-ovfunc = {'covSum',{SE,NO,LIN,CONS}};      %definition of covariance function
-hyp.cov = [hypSE;hypNO;hypLIN;hypCONS];
+%SE = {@covSEard};
+%L = rand(6,1); sf = 2; hypSE = log([L;sf]);
+%LIN = {'covLINard'};
+%L = rand(6,1); hypLIN = (L);
+%CONS = {@covConst};
+%sf = 2;  hypCONS = log(sf);
+%cf = {'covSum',{SE,LIN,CONS}};      %definition of covariance function
+%hyp.cov = [hypSE;hypLIN;hypCONS];
 
-%feval(likfunc{:})
-likfunc = {@likGauss};       %choose the likelihood function
-sn = 0.1;
-hyp.lik = log(sn);
+cf = @covSEard; ell = 100; sf = 5*std(yt); hyp.cov = [log(ell)*ones(6,1); log(sf)];
+mf = {@meanSum, {@meanConst, @meanLinear}}; c = 0.0; hyp.mean = [c; zeros(6,1)];
+lf = @likGauss; sn = std(yt); hyp.lik = log(sn);
 
-%Train the GPML
-[X, fX, i] = minimize(hyp, @gp, -1500, @infExact, meanfunc, covfunc, likfunc, Xt, yt);
-likelihood = X.lik %has to be <0
+% train the GPML (likGauss, meanZero)
+[X, fX, i] = minimize(hyp, @gp, -100, @infExact, mf, cf, lf, Xt, yt);
+likelihood = X.lik % has to be <0
 hyps = [X.cov];
-
-%Infer the IC50 value of TAS-120
-
-load Xtest.txt
 
 %xd = delay(xval,retardo);
 %xd2 = delay(xd,retardo);
 
-[m s2] = gp(X, @infExact, meanfunc, covfunc, likfunc, Xt, yt, Xtest);
-
+[m s2] = gp(X, @infExact, mf, cf, lf, Xt, yt, Xtest);
 m
